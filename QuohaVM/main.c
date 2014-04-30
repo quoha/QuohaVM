655373a6e3236854cd11bfde41def8946e5b09de
Michael Henderson <quoha@quoha.org>
2014-04-29 22:25:51 -0500

    initial commit from abbey

---
+++ QuohaVM/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  QuohaVM
+//
+//  Created by Michael Henderson on 4/29/14.
+//  Copyright (c) 2014 Maloquacious Productions, LLC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
