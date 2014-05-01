09e6e52ba41503bd190a3514869af8767563f62f
Michael Henderson <quoha@quoha.org>
2014-05-01 00:13:37 -0500

    update to Xcode 5

--- QuohaVM/main.c
+++ QuohaVM/main.c
@@ -7,10 +7,44 @@
 
 #include <stdio.h>
 
+typedef struct Cell Cell;
+typedef struct VM   VM;
+
+struct Cell {
+    union {
+        size_t        coreOffset;
+        long          integer;
+        double        number;
+        const char   *text;
+    } data;
+    enum { code, integer, null, number, text } kind;
+    enum {
+        opNOOP = 0,
+        opHALT,
+    } op;
+}; // struct Cell
+
+struct VM {
+    Cell  *programCounter;
+    Cell  *dataStack;
+    Cell  *returnStack;
+    Cell  *endOfCore;
+    Cell  *endOfStack;
+    Cell   core[1];
+}; // struct VM
+
+Cell *CellFromCell(Cell *cell);
+Cell *CellFromInteger(long integer);
+Cell *CellFromNull();
+Cell *CellFromNumber(double number);
+Cell *CellFromOffset(size_t offset);
+Cell *CellFromText(const char *text);
+
+
 int main(int argc, const char * argv[])
 {
     // insert code here...
-    printf("Hello, World!\n");
+    printf("Hello, World! %ld -> %ld %ld %ld\n", sizeof(Cell), sizeof(VM), sizeof(unsigned long), sizeof(unsigned long long));
     return 0;
 }
 
