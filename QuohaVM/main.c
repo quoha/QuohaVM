6fd11c4cf44746012e5ac8b396eb464d9e0789cc
Michael Henderson <quoha@quoha.org>
2014-05-01 00:26:44 -0500

    outline VM

--- QuohaVM/main.c
+++ QuohaVM/main.c
@@ -6,6 +6,7 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Cell Cell;
 typedef struct VM   VM;
@@ -24,13 +25,16 @@ struct Cell {
     } op;
 }; // struct Cell
 
+// core
+//  [ 0x0000 --> 0x???? ] endOfCore [ returnStack --> <-- dataStack ] endOfStack
+//
 struct VM {
     Cell  *programCounter;
     Cell  *dataStack;
     Cell  *returnStack;
     Cell  *endOfCore;
     Cell  *endOfStack;
-    Cell   core[1];
+    Cell   core[3];
 }; // struct VM
 
 Cell *CellFromCell(Cell *cell);
@@ -40,6 +44,17 @@ Cell *CellFromNumber(double number);
 Cell *CellFromOffset(size_t offset);
 Cell *CellFromText(const char *text);
 
+VM *NewVM(int kiloCells, int kiloStack);
+
+VM *NewVM(int kiloCells, int kiloStack) {
+    VM *vm = malloc(sizeof(*vm) + kiloCells * 1024 + kiloStack * 1024);
+    vm->programCounter = 0;
+    vm->endOfCore      = vm->core      + kiloCells * 1024;
+    vm->endOfStack     = vm->endOfCore + kiloStack * 1024;
+    vm->returnStack    = vm->endOfCore;
+    vm->dataStack      = vm->endOfStack;
+    return vm;
+}
 
 int main(int argc, const char * argv[])
 {
